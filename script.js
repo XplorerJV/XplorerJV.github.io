e3983cecd94e5d76d705ea8459d98b7985c8b970
Jayesh Verma <jayeshverma1912@gmail.com>
2025-12-02 16:26:42 +0530

    script.js

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ function toggleMenu(){
 // Certificate Gallery (Dynamic)
 const certificates = [
   {src:'cert1.jpg', title:'Java Certification', desc:'Completed advanced Java programming course.'},
-  {src:'cert2.jpg', title:'IoT Specialist', desc:'Certification in IoT and embedded systems projects.'},
-  {src:'cert3.jpg', title:'Python Developer', desc:'Completed Python automation & ML basics.'}
+  {src:'cert2.jpg', title:'IoT Specialist', desc:'Internship Certification in IoT and embedded systems projects.'},
+  {src:'cert3.jpg', title:'Google Certifiction', desc:'Completed the certification on The Fundamentals of Digital Marketing'}
 ];
 
 const certContainer=document.getElementById('certificates-container');
@@ -87,3 +87,4 @@ certificates.forEach(cert=>{
   card.innerHTML=`<img src="${cert.src}" alt="${cert.title}"><h3>${cert.title}</h3><p>${cert.desc}</p>`;
   certContainer.appendChild(card);
 });
+
